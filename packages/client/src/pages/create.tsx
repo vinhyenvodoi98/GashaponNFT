0a87acf259b4041c3aabe3d4568bb0cbee641e85
vinhyenvodoi98 <vinhyenvodoi98@gmail.com>
2025-01-25 18:25:18 +0900

    update create contract

--- packages/client/src/pages/create.tsx
+++ packages/client/src/pages/create.tsx
@@ -2,6 +2,8 @@
 
 import Loading from '@/components/Loading';
 import Wallet from '@/components/Providers/wallet';
+import UploadImage from '@/components/UploadImage';
+import { uploadWeb3Storage, web3StorageLink } from "@/services/web3Storage"
 import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import { useAccount } from 'wagmi';
@@ -10,6 +12,8 @@ export default function DeployContract() {
     const [image, setImage] = useState<File | null>(null);
     const [name, setName] = useState('');
     const [symbol, setSymbol] = useState('');
+    const [price, setPrice] = useState(0)
+    const [prompt, setPrompt] = useState('')
     const [status, setStatus] = useState(0)
     const [cid, setCid] = useState('')
     const [isSuccess, setIsSuccess] = useState(false)
@@ -18,13 +22,36 @@ export default function DeployContract() {
 
     const handleSubmit = async (e: React.FormEvent) => {
       e.preventDefault();
-      // await uploadContractData("")
+      setStatus(1) // start upload
+      const cid = await uploadWeb3Storage(image)
+      setCid(web3StorageLink(cid))
+      // sendAsync()
+      console.log(cid)
+      await uploadContractData(web3StorageLink(cid), "")
+      setStatus(0) // set contract
     };
+
+    const uploadContractData = async (cid:any, contractAddress:any) => {
+      const body = {
+        creator: address as string,
+        image: cid,
+        name: name,
+        symbol: symbol,
+        contractAddress,
+        prompt,
+        price
+      }
+      const bgResponse = await fetch('/api/collections', {
+        method: 'POST',
+        body: JSON.stringify(body),
+      });
+
+      console.log(bgResponse)
+    }
 
     return (
       <div className="min-h-main flex items-center justify-center">
         <Loading state={status} description='Creating contract'/>
-        {/* <button onClick={()=> getPublickey()}>Test</button> */}
         <div className="container mx-auto px-4 max-w-lg">
           <h1 className="text-4xl font-bold text-gray-800 text-center mb-4">
             Let's create a smart contract for your drop.
@@ -32,6 +59,14 @@ export default function DeployContract() {
           <p className='mb-4'>You will need to deploy an ERC-721 contract onto the blockchain before you can create a drop.</p>
 
           <form className="p-6 rounded-lg" onSubmit={handleSubmit}>
+
+            {/* Image Upload */}
+            <div className="mb-10">
+              <label htmlFor="image" className="block text-gray-700 font-semibold mb-2">
+                Upload logo Image
+              </label>
+              <UploadImage setFile={setImage}/>
+            </div>
             {/* Name Input */}
             <div className="mb-4">
               <label htmlFor="name" className="block text-gray-700 font-semibold mb-2">
@@ -59,6 +94,34 @@ export default function DeployContract() {
                 value={symbol}
                 onChange={(e) => setSymbol(e.target.value)}
                 placeholder="Enter contract symbol"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label htmlFor="symbol" className="block text-gray-700 font-semibold mb-2">
+                Prompt
+              </label>
+              <input
+                type="text"
+                id="prompt"
+                className="input input-bordered block w-full text-gray-700 border border-gray-300 p-2"
+                value={prompt}
+                onChange={(e) => setPrompt(e.target.value)}
+                placeholder="Enter NFT prompt"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label htmlFor="symbol" className="block text-gray-700 font-semibold mb-2">
+                Price
+              </label>
+              <input
+                type="text"
+                id="price"
+                className="input input-bordered block w-full text-gray-700 border border-gray-300 p-2"
+                value={price}
+                onChange={(e) => setPrice(Number(e.target.value))}
+                placeholder="Input price of each NFT"
               />
             </div>
 
